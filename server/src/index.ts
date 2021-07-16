3acf75a9089681ea376703cfe00da8581802ad91
Lucas <lucasalonso11800@gmail.com>
2021-07-16 12:10:48 -0300

    Connected to mongo

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import './.env';
 
 // import { schema } from './schema/schema';
 
@@ -14,6 +15,11 @@ app.use(express.json());
 //     schema,
 //     graphiql: true
 // }));
+
+mongoose.connect(process.env.MONGO_URI || '', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('Connected to db'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
